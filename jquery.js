dd7049c4dfb1f798d8ec569f4ff8f93bcbdea78e
栗原博之 <65903316+hiroyuki-kurihara@users.noreply.github.com>
2022-08-23 18:55:33 +0900

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,16 @@
+
+<script>
+    $(function() {
+    $(".home").click(function() {
+    $("div").fadeIn(3000);
+    });
+  });
+  </script>  
+
+
+    $(function() {
+      $("a").on("click",function() {
+        alert("アラート！！")
+    });
+        });
+
